293e99d7fdba1946f724ed294042f54c0cd72b81
Richard <rlew0925@gmail.com>
2024-11-17 17:07:45 -0700

    update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function Expression for divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow Function for square
+  const square = x => x ** 2;
+  
+  // Arrow Function for add
+  const add = (a, b) => a + b;
